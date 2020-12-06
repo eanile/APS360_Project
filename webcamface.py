59159ffd3f8ef8d2449c8b8def505e45326bb364
lucasrea <lucas.rea1998@gmail.com>
2020-12-05 23:31:08 -0500

    implemented model to webcam
    Implemented model prediction into live webcam facial recognition.
    Loads best model w/ 53% accuracy :  "model_KLARR_NET7_bs128_lr0.002_iter13_date23_11_2020-03_09"

--- webcamface.py
+++ webcamface.py
@@ -1,9 +1,68 @@
 import cv2
 import numpy as np
+import torch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+
+class KLARR_NET7(nn.Module):
+    def __init__(self):
+        super(KLARR_NET7, self).__init__()
+        self.name = 'KLARR_NET7'
+
+        self.batchnorm2 = nn.BatchNorm2d(32)
+        self.batchnorm5 = nn.BatchNorm2d(256)
+
+        self.maxpool = nn.MaxPool2d(2, 2)
+        self.avgpool = nn.AvgPool2d(2,2)
+
+        self.conv1 = nn.Conv2d(1, 16, 3, padding=2)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=2)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=2)
+        self.conv4 = nn.Conv2d(64, 128, 3, padding=2)
+        self.conv5 = nn.Conv2d(128, 256, 3, padding=2)
+
+        '''
+        48x48 (1ch) --> conv1 --> 50x50 (16ch)
+        50x50 (16ch) --> maxpool --> 25x25 (16ch)
+        25x25 (16ch) --> conv2 --> 27x27 (32ch)
+        27x27 (32ch) --> maxpool --> 13x13 (32ch)
+        13x13 (32ch) --> conv3 --> 15x15 (64ch)
+        15x15 (64ch) --> maxpool --> 7x7 (64ch)
+        7x7 (64ch) --> conv4 --> 9x9 (128ch)
+        9x9 (128ch) --> maxpool --> 4x4 (128ch)
+        4x4 (128ch) --> conv5 --> 6x6 (256ch)
+        6x6 (256ch) --> avgpool --> 3x3 (256ch)
+        '''
+
+        self.fc1 = nn.Linear(3*3*256, 50)
+        self.fc2 = nn.Linear(50, 6)
+
+    def forward(self, x):
+        # Convolutional layers
+        x = self.maxpool(F.relu(self.conv1(x)))
+        x = self.batchnorm2(self.maxpool(F.relu(self.conv2(x))))
+        x = self.maxpool(F.relu(self.conv3(x)))
+        x = self.maxpool(F.relu(self.conv4(x)))
+        x = self.batchnorm5(self.avgpool(F.relu(self.conv5(x))))
+
+        # Linear layers
+        x = x.view(-1,3*3*256)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)             # applies softmax activation on output
+
+        return x
 
 # Load the classifier and create a cascade object for face detection
 PATH_TO_VIOLA = 'ViolaJonesModels/haarcascade_frontalface_alt.xml'
 face_cascade = cv2.CascadeClassifier(PATH_TO_VIOLA)
+
+# Load the CNN model that will detect the facial expression
+MODEL_PATH = 'models/'
+model_final = KLARR_NET7()
+model_final.load_state_dict(torch.load("models/model_KLARR_NET7_bs128_lr0.002_iter13_date23_11_2020-03_09"))
 
 # Webcam display parameters
 WIDTH = 480
@@ -19,6 +78,9 @@ FONT = cv2.FONT_HERSHEY_PLAIN
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
+
+# Class list to determine what the model predicts
+classes = ['Anger', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']
 
 
 while(True):
@@ -39,11 +101,16 @@ while(True):
         face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
         face = cv2.resize(face, (MODEL_WIDTH, MODEL_HEIGHT))
         
-        # TODO pass in the face to the model and calculate the predicion accuracy / confidence
+        # Reshape the face so it it is 1 sample, with MODEL_WIDTH and  MODEL_HEIGHT dimensions, and 1 channel
+        input_face_to_model = np.reshape(face, (1,MODEL_WIDTH, MODEL_HEIGHT,1)).astype(np.float32) / 255
+
+        model_output = model_final(torch.tensor(input_face_to_model).permute(0, 3, 1, 2))
+
+        predicion = classes[np.argmax(model_output.detach().numpy())]
 
         cv2.putText(
             frame_image, 
-            'This is a face!',  # Replace w/ model prediction
+            predicion,  # Replace w/ model prediction
             (column, row), 
             FONT, 
             FONT_SCALE, 
